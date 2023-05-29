6f93ce2d4497e86f3eb64acdfad1ac5db3944920
catherinebah <114077354+catherinebah@users.noreply.github.com>
2023-05-29 17:14:35 +0200

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -9,6 +9,7 @@ Original file is located at
 
 # Import libraries
 import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
